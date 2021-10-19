66d81e0fbaa3fee2afc5dcd5f8ef59d41c38007d
vini404 <vinicius404contato@gmail.com>
2021-10-19 16:16:16 -0300

    Projeto teste

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+namespace teste
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double desconto = (4 > 3) ? 1.0 : 3.0;
+
+
+            Console.WriteLine(desconto);
+        }
+
+    }
+
+
+
+
+
+    }
+
---
+++ pessoa.cs
@@ -0,0 +1,14 @@
+namespace teste
+{
+    public class pessoa
+    {
+
+        private string name;
+        private string lastname;
+
+        private int age;
+
+
+        public pessoa 
+    }
+}
